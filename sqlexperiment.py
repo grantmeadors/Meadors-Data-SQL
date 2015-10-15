c1a7cb2d23488813745d936c21c82c06fb88eebd
Grant David Meadors <grant.meadors@ligo.org>
2015-10-15 17:20:48 +0200

    Experimenting with SQL databases

--- sqlexperiment.py
+++ sqlexperiment.py
@@ -7,4 +7,22 @@
 
 import sqlite3
 
-#print 'Hello, Pythonistas!'
+conn = sqlite3.connect('exampleTSoutput.db')
+# Create a connection
+c = conn.cursor()
+
+c.execute('''CREATE TABLE twospectout
+             (fsig real, period real, df real, ra real, dec real, r real, h0 real, prob real, tfnorm real)''')
+
+templates = [
+            (74.043000, 68023.825900, 0.0133342, 4.2757, -0.2730, 93.9088, 1.10763e-24, -4.8475, 3.49802e+45),
+            (74.043000, 68023.825900, 0.0136296, 4.2757, -0.2730, 94.3590, 1.10895e-24, -4.5989, 3.49802e+45),
+            (74.043000, 68023.825900, 0.0139251, 4.2757, -0.2730, 101.6311, 1.12973e-24, -4.8166, 3.49802e+45)
+             ]
+c.executemany('INSERT INTO twospectout VALUES (?,?,?,?,?,?,?,?,?)', templates)
+
+for row in c.execute('SELECT * FROM twospectout ORDER BY fsig'):
+    print row
+
+conn.close()
+
