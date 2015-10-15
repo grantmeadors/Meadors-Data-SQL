a834010cbc5f03c8c351debb8fc16a4d6a55b142
Grant David Meadors <grant.meadors@ligo.org>
2015-10-15 16:58:24 +0200

    Initial commit on SQL experiments. Grant David Meadors, 02015-10-15 (JD 2457311)

---
+++ sqlexperiment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# Grant David Meadors
+# 02015-10-15 (JD 2457311)
+# g r a n t . m e a d o r s @ a e i . m p g . d e
+# Experiment to learn how to manipulate frames in SQL using Python,
+# later to be adapted to R
+
+import sqlite3
+
+#print 'Hello, Pythonistas!'
