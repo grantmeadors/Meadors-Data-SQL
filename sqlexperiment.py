87a13c55f1a9e24d034c1854ab4a038085548f72
Grant David Meadors <grant.meadors@ligo.org>
2015-10-15 17:24:20 +0200

    Now we need to load the rpy2 module

--- sqlexperiment.py
+++ sqlexperiment.py
@@ -11,18 +11,25 @@ conn = sqlite3.connect('exampleTSoutput.db')
 # Create a connection
 c = conn.cursor()
 
+# Format the database
 c.execute('''CREATE TABLE twospectout
              (fsig real, period real, df real, ra real, dec real, r real, h0 real, prob real, tfnorm real)''')
 
+# Specify example values
 templates = [
             (74.043000, 68023.825900, 0.0133342, 4.2757, -0.2730, 93.9088, 1.10763e-24, -4.8475, 3.49802e+45),
             (74.043000, 68023.825900, 0.0136296, 4.2757, -0.2730, 94.3590, 1.10895e-24, -4.5989, 3.49802e+45),
             (74.043000, 68023.825900, 0.0139251, 4.2757, -0.2730, 101.6311, 1.12973e-24, -4.8166, 3.49802e+45)
              ]
+# Actually insert values into table
 c.executemany('INSERT INTO twospectout VALUES (?,?,?,?,?,?,?,?,?)', templates)
 
+# Print the values
 for row in c.execute('SELECT * FROM twospectout ORDER BY fsig'):
     print row
 
+# Close connection
 conn.close()
 
+# NEXT, see if we can import an R data frame
+#from rpy2.robjects import pandas2ri
